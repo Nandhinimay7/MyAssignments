b9c9f13d8e75ac2d73e20b8f093ebf58c44633df
Nandhinimay7 <110736666+Nandhinimay7@users.noreply.github.com>
2022-08-06 22:42:50 +0530

    Add files via upload
    Day  1 Assignments

---
+++ day1/Car.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Car {
+	
+	byte maxSpeed = 120;
+	short discountedPrice = -32555;
+	int fuelCapacity = 42;
+	long vinNumber = 12345678901234567L;
+	float price = 1450000.505F;
+	double distanceTravelled = 1234567890;;
+	boolean isFuelFull = true;
+	char model = 'A';
+	
+	String brand = "Mahindra";
+	
+
+}
